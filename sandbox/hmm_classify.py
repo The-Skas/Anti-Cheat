ecb45e1c2f61962a096af5220880d33fcb6ad8b9
The-Skas <skas@live.co.uk>
2016-08-10 20:06:56 +0100

    Finally can get multi columns HMM working.

--- sandbox/hmm_classify.py
+++ sandbox/hmm_classify.py
@@ -14,19 +14,32 @@ import time
 
 def window_stack(a, stepsize=1, width=3):
     n = a.shape[0]
-    _dim = 1 if len(windows.shape) < 2 else windows.shape[1]
+    _dim = 1 if len(a.shape) < 2 else a.shape[1]
     if _dim == 1:
     	return np.hstack( a[i:1+n+i-width:stepsize] for i in range(0,width) )
     else:
-    	return np.vstack( a[i:1+n+i-width:stepsize] for i in range(0,width) )
+    	length = n 
+    	remainder = n % width 
+    	result = np.stack(a[i:width+i] for i in range(0,length-width))
+    	if remainder == 0:
+    		return result 
+    	else:
+    		# gets the remainder and stacks it.
+    		## Todo: sort out missing data.
+    		#return np.vstack([result, a[-remainder:]])
+    		return result
 
 
 def sliding_window_predict(test_data,model_fair, model_hacker, window_size=128):
 	windows = window_stack(test_data, width=window_size)
-	
-	_dim = 1 if len(windows.shape) < 2 else windows.shape[1]
-	scores_fair = np.empty(( len(windows) , _dim), float)
-	scores_hacker = np.empty(( len(windows) , _dim), float)
+
+	# Uh... Don't like how this looks like as it can error.
+	_dim = 1 if len(windows[0].shape) < 2 else windows[0].shape[1]
+
+	# Scores should be array X by 1 Dimensions
+	scores_fair = np.empty(( len(windows) , 1), float)
+	scores_hacker = np.empty(( len(windows) , 1), float)
+
 	for i, window in enumerate(windows):
 		scores_fair[i] =   model_fair.score(window.reshape(len(window),  _dim))
 		scores_hacker[i] = model_hacker.score(window.reshape(len(window), _dim))
@@ -66,13 +79,18 @@ def classify_analysis(dfhacker, dffair, columns, n_components=4):
 
 	length_diff = len(test_hacker) - len(diff_scores)
 	#
-	y = np.vstack([np.zeros((length_diff,1)), diff_scores ])
+
+	## Should be 1 dimensoional as were simply stacking 0s on the score
+	y = np.vstack([np.zeros((length_diff, 1)), diff_scores ])
 
 	# Dont panic just reshaping stuff
 	x = test_round_h[0]["Tick"].as_matrix()
 	x = x.reshape(len(x), 1)
 
 	csgo_plot.plot_plane_diff(x, y, title="n-"+str(n_components)+"_".join(columns))
+
+	pdb.set_trace()
+	print "Stop"
 
 
 
@@ -85,7 +103,7 @@ fairargs = {'id':76561197979669175, 'class':'fair', 'start_tick':60000, 'end_tic
 
 dffair = clean_data_to_numbers("128t_de_inferno_186_envyus-dignitas_de_inferno.csv", "128t_de_inferno_186_envyus-dignitas_de_inferno_attackinfo.csv", dictargs=fairargs)
 
-classify_analysis(dfhacker, dffair, ["TrueViewDiffSpeed","TrueViewRadDiff"])
+classify_analysis(dfhacker, dffair, ["TrueViewDiffSpeed","TrueViewRadDiff"], n_components=12)
 # classify_analysis(dfhacker, dffair, ["TrueViewRadDiff"])
 
 
